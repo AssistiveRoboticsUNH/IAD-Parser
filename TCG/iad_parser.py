1a1e89b92a59cf5734ce1118d9a434552defabf9
MClarkTurner <mclarktu@fandm.edu>
2019-11-24 13:05:09 -0500

    iad_parser cleanup

--- TCG/iad_parser.py
+++ TCG/iad_parser.py
@@ -152,7 +152,7 @@ def main(dataset_dir, csv_filename, dataset_type, dataset_id, feature_retain_cou
 
 	#setup feature_rank_parser
 	iad_data_path = os.path.join(dataset_dir, 'iad_'+dataset_type+'_'+str(dataset_id))
-	ranking_file = os.path.join(iad_data_path_frames, "feature_ranks_"+str(dataset_id)+".npz")
+	ranking_file = os.path.join(iad_data_path, "feature_ranks_"+str(dataset_id)+".npz")
 	assert os.path.exists(ranking_file), "Cannot locate Feature Ranking file: "+ ranking_file
 	pruning_indexes = get_top_n_feature_indexes(ranking_file, feature_retain_count)
 	
