fdbb837f68fd6a5fe727f206a67e6aa8c47f58f3
MClarkTurner <mclarktu@fandm.edu>
2019-12-17 14:04:18 -0500

    preprocessing check

--- TCG/iad_parser.py
+++ TCG/iad_parser.py
@@ -20,7 +20,7 @@ def preprocess(iad):
 def find_start_stop(feature, iad, layer):
 
 	# smooth the IAD expression
-	if(layer < 3):
+	if(iad.shape[1] > 25):
 		#w_size =  (iad.shape[1]/2) if (iad.shape[1]/2) % 2 != 0 else (iad.shape[1]/2)-1
 		feature = savgol_filter(feature, 25, 3)  ## 25
 	
