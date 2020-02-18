4d7976e671dd2b68db71343efd9832afeceeee6f
MClarkTurner <mclarktu@fandm.edu>
2020-02-17 23:44:19 -0500

    multithreading

--- TCG/iad_parser_mt.py
+++ TCG/iad_parser_mt.py
@@ -175,8 +175,8 @@ def sparsify_iad(ex, layer, dataset_type_list, threshold_matrix, name="output.tx
 
 	# threshold, reverse the locations to account for the transpose
 	locs = np.where(iad.T > threshold_values)
-	print(iad)
-	print(locs)
+	#print(iad)
+	#print(locs)
 	locs = np.array( zip( locs[1], locs[0] ) )
 
 	sparse_map = []
@@ -197,8 +197,8 @@ def sparsify_iad(ex, layer, dataset_type_list, threshold_matrix, name="output.tx
 	
 	smx = read_sparse_matrix(ex['txt_path_{0}'.format(layer)])
 
-	print("smx")
-	print(smx)
+	#print("smx")
+	#print(smx)
 	
 
 
