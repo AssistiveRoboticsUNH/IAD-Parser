3c91c5fe6e6a50914e75ecdcfcaab8e8bd8be920
MClarkTurner <mclarktu@fandm.edu>
2020-02-20 14:18:39 -0500

    trn remove preprocessing

--- TCG/iad_parser_mt.py
+++ TCG/iad_parser_mt.py
@@ -115,7 +115,7 @@ def sparsify_iad(ex, layer, dataset_type_list, threshold_matrix, num_features, n
 	# get the start and stop times for each feature in the IAD
 	sparse_map = []
 	for i in range(iad.shape[0]):
-		assert len(locs) != 0, "ERROR: couldn't find Locs: " + ex["example_id"]
+		assert len(locs) != 0, "ERROR: couldn't find Locs: " + ex["iad_path_frames_"+str(layer)]
 
 		feature_times = locs[np.where(locs[:,0] == i)][:,1]
 		sparse_map.append( find_start_stop( feature_times ))
